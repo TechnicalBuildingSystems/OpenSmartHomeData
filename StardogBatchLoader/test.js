b551050bfd1a3c31342e1f41e4853cee9aa0abb0
Mads Holten Rasmussen <mads.h.rasmussen@gmail.com>
2018-05-13 20:19:28 +0200

    Corrected errors in 00_OpenSmartHomeData.ttl

--- StardogBatchLoader/test.js
+++ StardogBatchLoader/test.js
@@ -15,7 +15,7 @@ var conn = new Connection({
 });
 var dbName = config.database;
 
-var filePaths;
+var filePaths = ['../00_OpenSmartHomeData.ttl'];
 var ttlStrings;
 
 describe('Test Stardog connection', () => {
@@ -41,107 +41,23 @@ describe('Test Stardog connection', () => {
 });
 
 describe('Load data in triplestore', () => {
-    
-        it('Read all turtle files into array of strings', async () => {
+
+    it(`Load data in store`, async () => {
+
+        for(let file of config.filePaths){
+            // Load in store
+            // file = '../00_OpenSmartHomeData.ttl'
+            const triples = await fs.readFile(file, 'utf8');
+            var res = await db.graph.doPost(conn, dbName, triples, undefined, 'text/turtle');
+            expect(res).to.have.property('status').that.is.equals(200);
             
-            // Read all turtle files into strings in parallel
-            readFilePromises = [];
-            filePaths = [];
+            // Perform check
+            const q = 'SELECT (count(?s) AS ?count) WHERE {?s ?p ?o}';
+            res = await query.execute(conn, dbName, q);
+            const cnt =res.body.results.bindings[0].count.value;
+            console.log(`\t${cnt}\ttriples in store [${file}]`);
+        }
 
-            for(let filePath of config.filePaths){
-                var files = await fs.readdir(filePath);
-
-                for(let file of files){
-                    var fileType = path.extname(file);
-
-                    if(fileType == '.ttl'){
-                        var f = path.join(filePath,file);
-                        filePaths.push(f);
-                        readFilePromises.push(fs.readFile(f, 'utf8'));
-                    }
-                    
-                }
-            }
-
-            this.ttlStrings = await Promise.all(readFilePromises);
-            this.filePaths = filePaths;
-
-            console.log(`\t- created array with ${filePaths.length} Turtle strings`);
-
-            expect(this.ttlStrings).to.be.an('array').to.not.be.empty;
-
-        });
-
-        it(`Load triples in store via HTTP`, async () => {
-            var i = 0;
-            for(let triples of this.ttlStrings){
-                var res = await db.graph.doPost(conn, dbName, triples, undefined, 'text/turtle');
-                this.ttlStrings.shift();
-
-                expect(res).to.have.property('status').that.is.equals(200);
-
-                const q = 'SELECT (count(?s) AS ?count) WHERE {?s ?p ?o}';
-                res = await query.execute(conn, dbName, q);
-                const cnt =res.body.results.bindings[0].count.value;
-                console.log(`\t${i+1}:\t${cnt}\ttriples in store [${this.filePaths[i]}]`);
-                i+=1;
-            }
-        });
-
-        // it(`Load triples in store via CMD`, async () => {
-        //     var i = 0;
-        //     for(let file of this.filePaths){
-
-        //         // The command to run
-        //         const cmd = `stardog data add ${dbName} ${file}`;
-    
-        //         // Execute command 
-        //         try{
-        //             let { stdout, stderr } = await sh(cmd);
-        //             res = stdout;
-        //         }
-        //         catch(err){
-        //             expect(err).to.be(null);
-        //         }
-
-        //         this.filePaths.shift();
-
-        //         const q = 'SELECT (count(?s) AS ?count) WHERE {?s ?p ?o}';
-        //         res = await query.execute(conn, dbName, q);
-        //         const cnt =res.body.results.bindings[0].count.value;
-        //         console.log(`\t${i+1}:\t${cnt}\ttriples in store [${this.filePaths[i]}]`);
-        //         i+=1;
-        //     }
-        // });
-
-        // it(`Load 01_LinkOsh again`, async () => {
-        //     const f = '../01_LinkOsh.ttl';
-        //     const triples = await fs.readFile(f, 'utf8');
-        //     var res = await db.graph.doPost(conn, dbName, triples, undefined, 'text/turtle');
-
-        //     expect(res).to.have.property('status').that.is.equals(200);
-
-        //     const q = 'SELECT (count(?s) AS ?count) WHERE {?s ?p ?o}';
-        //     res = await query.execute(conn, dbName, q);
-        //     const cnt =res.body.results.bindings[0].count.value;
-
-        //     console.log(`\tstore now has ${cnt} triples`);
-        // });
-
-        // it(`Load 02_GeoFromRevit again`, async () => {
-        //     const f = '../02_GeoFromRevit.ttl';
-        //     const triples = await fs.readFile(f, 'utf8');
-        //     var res = await db.graph.doPost(conn, dbName, triples, undefined, 'text/turtle');
-
-        //     expect(res).to.have.property('status').that.is.equals(200);
-
-        //     const q = 'SELECT (count(?s) AS ?count) WHERE {?s ?p ?o}';
-        //     res = await query.execute(conn, dbName, q);
-        //     const cnt =res.body.results.bindings[0].count.value;
-
-        //     console.log(`\tstore now has ${cnt} triples`);
-        // });
-
-
+    });
 
 });
